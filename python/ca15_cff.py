a8050ca09beb27d1a88ec9b3e6976f1912aba0f5
Huilin Qu <huilin.qu@cern.ch>
2018-08-15 12:18:48 +0200

    Add jet ID for CA15.

--- python/ca15_cff.py
+++ python/ca15_cff.py
@@ -12,6 +12,8 @@ def setupCA15(process, runOnMC=False, path=None):
     ]
     subjetBTagDiscriminators = [
         'pfCombinedInclusiveSecondaryVertexV2BJetTags',
+        'pfDeepCSVJetTags:probb',
+        'pfDeepCSVJetTags:probbb',
     ]
     JETCorrLevels = ['L2Relative', 'L3Absolute', 'L2L3Residual']
 
@@ -31,14 +33,57 @@ def setupCA15(process, runOnMC=False, path=None):
         bdt_name=cms.untracked.string('v0'),
     )
 
+    # src
+    srcJets = cms.InputTag('ecfTopTagCA15Puppi')
+
+    # jetID
+    process.looseJetIdCA15Puppi = cms.EDProducer("PatJetIDValueMapProducer",
+              filterParams=cms.PSet(
+                version=cms.string('WINTER16'),
+                quality=cms.string('LOOSE'),
+              ),
+              src=srcJets
+    )
+
+    process.tightJetIdCA15Puppi = cms.EDProducer("PatJetIDValueMapProducer",
+              filterParams=cms.PSet(
+                version=cms.string('WINTER17'),
+                quality=cms.string('TIGHT'),
+              ),
+              src=srcJets
+    )
+    run2_miniAOD_80XLegacy.toModify(process.tightJetIdCA15Puppi.filterParams, version="WINTER16")
+
+    process.tightJetIdLepVetoCA15Puppi = cms.EDProducer("PatJetIDValueMapProducer",
+              filterParams=cms.PSet(
+                version=cms.string('WINTER17'),
+                quality=cms.string('TIGHTLEPVETO'),
+              ),
+              src=srcJets
+    )
+
+    process.ca15WithUserData = cms.EDProducer("PATJetUserDataEmbedder",
+        src=srcJets,
+        userFloats=cms.PSet(),
+        userInts=cms.PSet(
+           tightId=cms.InputTag("tightJetIdCA15Puppi"),
+           tightIdLepVeto=cms.InputTag("tightJetIdLepVetoCA15Puppi"),
+        ),
+    )
+    run2_miniAOD_80XLegacy.toModify(process.ca15WithUserData.userInts,
+        looseId=cms.InputTag("looseJetIdCA15Puppi"),
+        tightIdLepVeto=None,
+    )
+
     process.ca15Table = cms.EDProducer("SimpleCandidateFlatTableProducer",
-        src=cms.InputTag("ecfTopTagCA15Puppi"),
+        src=cms.InputTag("ca15WithUserData"),
         name=cms.string("CA15Puppi"),
         cut=cms.string(""),
         doc=cms.string("ca15 puppi jets"),
         singleton=cms.bool(False),  # the number of entries is variable
         extension=cms.bool(False),  # this is the main table for the jets
         variables=cms.PSet(P4Vars,
+            jetId=Var("userInt('tightId')*2+4*userInt('tightIdLepVeto')", int, doc="Jet ID flags bit1 is loose (always false in 2017 since it does not exist), bit2 is tight, bit3 is tightLepVeto"),
             area=Var("jetArea()", float, doc="jet catchment area, for JECs", precision=10),
             rawFactor=Var("1.-jecFactor('Uncorrected')", float, doc="1 - Factor to get back to raw pT", precision=6),
             msoftdrop=Var("groomedMass()", float, doc="Corrected soft drop mass with PUPPI", precision=10),
@@ -49,12 +94,12 @@ def setupCA15(process, runOnMC=False, path=None):
                  doc="index of second subjet"),
             # DeepAK8: nominal
             ecf0=Var("userFloat('ecf_0')", float, doc="ecfN_1_2_20/pow(ecfN_1_2_10,2.00)", precision=10),
-            httMass=Var("userFloat('httMass')", float, doc="HTT mass", precision=10),
             httFRec=Var("userFloat('httFRec')", float, doc="HTT frec", precision=10),
             tau32sd=Var("userFloat('tau32sd')", float, doc="soft drop tau32", precision=10),
             ecfTopTagBDT=Var("userFloat('ecfTopTagBDT')", float, doc="ECF top BDT", precision=10),
         )
     )
+    run2_miniAOD_80XLegacy.toModify(process.ca15Table.variables, jetId=Var("userInt('tightId')*2+userInt('looseId')", int, doc="Jet ID flags bit1 is loose, bit2 is tight"))
     process.ca15Table.variables.pt.precision = 10
 
     process.ca15SubJetTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
@@ -65,6 +110,7 @@ def setupCA15(process, runOnMC=False, path=None):
         singleton=cms.bool(False),  # the number of entries is variable
         extension=cms.bool(False),  # this is the main table for the jets
         variables=cms.PSet(P4Vars,
+            btagDeepB=Var("bDiscriminator('pfDeepCSVJetTags:probb')+bDiscriminator('pfDeepCSVJetTags:probbb')", float, doc="DeepCSV b+bb tag discriminator", precision=10),
             btagCSVV2=Var("bDiscriminator('pfCombinedInclusiveSecondaryVertexV2BJetTags')", float, doc=" pfCombinedInclusiveSecondaryVertexV2 b-tag discriminator (aka CSVV2)", precision=10),
             rawFactor=Var("1.-jecFactor('Uncorrected')", float, doc="1 - Factor to get back to raw pT", precision=6),
             area=Var("jetArea()", float, doc="jet catchment area, for JECs", precision=10),
@@ -75,8 +121,15 @@ def setupCA15(process, runOnMC=False, path=None):
     process.ca15Task = cms.Task(
         process.ecfTopTagCA15Puppi,
         process.ca15Table,
+        process.tightJetIdCA15Puppi,
+        process.tightJetIdLepVetoCA15Puppi,
+        process.ca15WithUserData,
         process.ca15SubJetTable
         )
+
+    _ca15Task_80X = process.ca15Task.copy()
+    _ca15Task_80X.replace(process.tightJetIdLepVetoCA15Puppi, process.looseJetIdCA15Puppi)
+    run2_miniAOD_80XLegacy.toReplaceWith(process.ca15Task, _ca15Task_80X)
 
     if path is None:
         process.schedule.associate(process.ca15Task)
