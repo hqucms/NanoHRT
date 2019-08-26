dc64d6e34ce76373bc4452a5629a34811e5348b4
Cristina <criss.ms7@gmail.com>
2019-08-26 17:36:49 -0500

    modify precision

--- python/ak8_cff.py
+++ python/ak8_cff.py
@@ -220,13 +220,13 @@ def setupCustomizedAK8(process, runOnMC=False, path=None):
                                                         singleton = cms.bool(False), # the number of entries is variable
                                                         extension = cms.bool(False), # this is the main table for the AK8 constituents
                                                         variables = cms.PSet(CandVars,
-                                                                             puppiWeight = Var("puppiWeight()", float, doc="Puppi weight",precision=20),
-                                                                             puppiWeightNoLep = Var("puppiWeightNoLep()", float, doc="Puppi weight removing leptons",precision=20),
-                                                                             vtxChi2 = Var("?hasTrackDetails()?vertexChi2():-1", float, doc="vertex chi2",precision=20),
-                                                                             trkChi2 = Var("?hasTrackDetails()?pseudoTrack().normalizedChi2():-1", float, doc="normalized trk chi2", precision=20),
-                                                                             dz = Var("?hasTrackDetails()?dz():-1", float, doc="pf dz",precision=20),
-                                                                             d0 = Var("?hasTrackDetails()?dxy():-1", float, doc="pf d0",precision=20),
-                                                                             d0Err = Var("?hasTrackDetails()?dxyError():-1", float, doc="pf d0 err",precision=20),
+                                                                             puppiWeight = Var("puppiWeight()", float, doc="Puppi weight",precision=10),
+                                                                             puppiWeightNoLep = Var("puppiWeightNoLep()", float, doc="Puppi weight removing leptons",precision=10),
+                                                                             vtxChi2 = Var("?hasTrackDetails()?vertexChi2():-1", float, doc="vertex chi2",precision=10),
+                                                                             trkChi2 = Var("?hasTrackDetails()?pseudoTrack().normalizedChi2():-1", float, doc="normalized trk chi2", precision=10),
+                                                                             dz = Var("?hasTrackDetails()?dz():-1", float, doc="pf dz",precision=10),
+                                                                             d0 = Var("?hasTrackDetails()?dxy():-1", float, doc="pf d0",precision=10),
+                                                                             d0Err = Var("?hasTrackDetails()?dxyError():-1", float, doc="pf d0 err",precision=10),
                                                                              )
                                                         )
 
