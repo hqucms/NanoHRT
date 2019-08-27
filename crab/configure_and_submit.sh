9fa9559d044e33444328b9f31aad47f34ee21756
Nick Smith <nick.smith@cern.ch>
2019-08-27 17:01:51 -0500

    CRAB testing setup

---
+++ crab/configure_and_submit.sh
@@ -0,0 +1,6 @@
+cmsDriver.py test_nanoHRT_mc -n 1000 --mc --eventcontent NANOAODSIM --datatier NANOAODSIM \
+  --conditions 102X_mc2017_realistic_v7 --step NANO --nThreads 4 --era Run2_2017,run2_nanoAOD_94XMiniAODv2 \
+  --customise PhysicsTools/NanoHRT/nanoHRT_cff.nanoHRT_customizeMC --filein file:step-1.root --fileout file:nano_mc.root \
+  --customise_commands "process.options = cms.untracked.PSet ( wantSummary = cms.untracked.bool ( True ) )" --no_exec
+python crab.py -p test_nanoHRT_mc_NANO.py -o /store/user/ncsmith/pancakes/01/ -t pancakes-01 -i mc_2017.txt \
+  --num-cores 4 --send-external -s EventAwareLumiBased -n 50000 --work-area crab_projects_mc_2017 --dryrun 
