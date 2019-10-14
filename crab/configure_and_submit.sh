33f339b77ad83aba120ae8b7e0b52484478451f1
Nick Smith <nick.smith@cern.ch>
2019-10-14 13:37:53 -0500

    Add bacon softdrop, document bacon DDX

--- crab/configure_and_submit.sh
+++ crab/configure_and_submit.sh
@@ -1,8 +1,9 @@
 cmsDriver.py test_nanoHRT_mc -n 1000 --mc --eventcontent NANOAODSIM --datatier NANOAODSIM \
   --conditions 102X_mc2017_realistic_v7 --step NANO --nThreads 4 --era Run2_2017,run2_nanoAOD_94XMiniAODv2 \
   --customise PhysicsTools/NanoHRT/nanoHRT_cff.nanoHRT_customizeMC --fileout file:nano_mc.root \
-  --filein dummy.root # "dbs:/QCD_HT700to1000_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v2/MINIAODSIM" \
+  --filein "dbs:/ZJetsToQQ_HT600to800_qc19_4j_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM" \
   --no_exec # --customise Configuration/DataProcessing/Utils.addMonitoring
 
 python crab.py -p test_nanoHRT_mc_NANO.py -o /store/user/ncsmith/pancakes/01/ -t pancakes-01 -i mc_2017.txt \
-  --num-cores 4 --max-memory 8000 --send-external -s EventAwareLumiBased -n 100000 --work-area crab_projects_mc_2017 # --dryrun
+  --num-cores 4 --max-memory 6000 --send-external -s EventAwareLumiBased -n 20000 --work-area crab_projects_mc_2017 \
+  --dryrun
