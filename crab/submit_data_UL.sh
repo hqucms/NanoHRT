b6684d945e9052e85c683e251442172e1355d344
Cristina <criss.ms7@gmail.com>
2020-01-22 18:58:42 -0600

    add dryrun

--- crab/submit_data_UL.sh
+++ crab/submit_data_UL.sh
@@ -8,8 +8,7 @@ cmsDriver.py pancakes_2017_data_UL -n -1 --data --eventcontent NANOAOD --datatie
 python crab.py -p pancakes_2017_data_UL_NANO.py -o /store/group/lpcbacon/pancakes/02/2017/ -t pancakes-02 -i data_2017_UL.txt  --num-cores 2 --send-external \
     -s EventAwareLumiBased \
     -n 200000 --work-area crab_projects_data_2017 --max-memory 5000 \
-    --json https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions18/13TeV/ReReco/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt 
-#--dryrun
+    --json https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions18/13TeV/ReReco/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt --dryrun
 
 # cmsDriver.py pancakes_2018_data_UL -n -1 --data --eventcontent NANOAOD --datatier NANOAOD --conditions 106X_dataRun2_v20 --step NANO --nThreads 2 \
 #     --era Run2_2018,run2_nanoAOD_106Xv1 \
--- crab/submit_mc_UL.sh
+++ crab/submit_mc_UL.sh
@@ -5,7 +5,7 @@ cmsDriver.py pancakes_2017_mc_UL -n 100 --mc --eventcontent NANOAODSIM --datatie
     --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring
 
 python crab.py -p pancakes_2017_mc_UL_NANO.py -o /store/group/lpcbacon/pancakes/02/2017/ -t pancakes-02 -i mc_2017_UL.txt --num-cores 4  -s EventAwareLumiBased \
-    -n 100000 --work-area crab_projects_mc_2017 --max-memory 5000 
+    -n 100000 --work-area crab_projects_mc_2017 --max-memory 5000 --dryrun
 
 cmsDriver.py pancakes_2017_mc -n 100 --mc --eventcontent NANOAODSIM --datatier NANOAODSIM --conditions 102X_mc2017_realistic_v7 --step NANO --nThreads 4 \
     --era Run2_2017,run2_nanoAOD_94XMiniAODv2 --customise PhysicsTools/Pancakes/nanoHRT_cff.nanoHRT_customizeMC \
@@ -14,4 +14,4 @@ cmsDriver.py pancakes_2017_mc -n 100 --mc --eventcontent NANOAODSIM --datatier N
     --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring
 
 python crab.py -p pancakes_2017_mc_NANO.py -o /store/group/lpcbacon/pancakes/02/2017/ -t pancakes-02 -i mc_2017.txt \
-    --num-cores 4 --max-memory 6000 --send-external -s EventAwareLumiBased -n 50000 --work-area crab_projects_mc_2017 
+    --num-cores 4 --max-memory 6000 --send-external -s EventAwareLumiBased -n 50000 --work-area crab_projects_mc_2017 --dryrun
