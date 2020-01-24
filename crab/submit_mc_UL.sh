c7616dbd9c4bc636321d0d4d9b887454c673b2db
Cristina Ana Mantilla Suarez <cmantill@users.noreply.github.com>
2020-01-24 11:45:00 -0600

    another UL dir

--- crab/submit_mc_UL.sh
+++ crab/submit_mc_UL.sh
@@ -4,7 +4,7 @@ cmsDriver.py pancakes_2017_mc_UL -n 100 --mc --eventcontent NANOAODSIM --datatie
     --filein file:miniAOD.root --fileout file:nano_mc_2017.root \
     --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring
 
-python crab.py -p pancakes_2017_mc_UL_NANO.py -o /store/group/lpcbacon/pancakes/02/2017/ -t pancakes-02 -i mc_2017_UL.txt --num-cores 4  -s EventAwareLumiBased \
+python crab.py -p pancakes_2017_mc_UL_NANO.py -o /store/group/lpcbacon/pancakes/02/2017/UL/ -t pancakes-02 -i mc_2017_UL.txt --num-cores 4  -s EventAwareLumiBased \
     -n 100000 --work-area crab_projects_mc_2017 --max-memory 5000 --dryrun
 
 cmsDriver.py pancakes_2017_mc -n 100 --mc --eventcontent NANOAODSIM --datatier NANOAODSIM --conditions 102X_mc2017_realistic_v7 --step NANO --nThreads 4 \
