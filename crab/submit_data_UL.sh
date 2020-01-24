3425d31dfaaac06d88ef7397366c2430b79e06a0
Cristina Ana Mantilla Suarez <cmantill@users.noreply.github.com>
2020-01-24 11:33:52 -0600

    add UL dir

--- crab/submit_data_UL.sh
+++ crab/submit_data_UL.sh
@@ -5,7 +5,7 @@ cmsDriver.py pancakes_2017_data_UL -n -1 --data --eventcontent NANOAOD --datatie
     --filein file:miniAOD.root --fileout file:nano_data_2017.root \
     --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring
 
-python crab.py -p pancakes_2017_data_UL_NANO.py -o /store/group/lpcbacon/pancakes/02/2017/ -t pancakes-02 -i data_2017_UL.txt  --num-cores 2 --send-external \
+python crab.py -p pancakes_2017_data_UL_NANO.py -o /store/group/lpcbacon/pancakes/02/2017/UL/ -t pancakes-02 -i data_2017_UL.txt  --num-cores 2 --send-external \
     -s EventAwareLumiBased \
     -n 200000 --work-area crab_projects_data_2017 --max-memory 5000 \
     --json https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions18/13TeV/ReReco/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt --dryrun
@@ -17,7 +17,7 @@ python crab.py -p pancakes_2017_data_UL_NANO.py -o /store/group/lpcbacon/pancake
 #     --filein file:miniAOD.root --fileout file:nano_data_2018.root \
 #     --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring
 
-# python crab.py -p pancakes_2018_data_UL_NANO.py -o /store/group/lpcbacon/pancakes/02/2018/ -t pancakes-02 -i data_2018_UL.txt  --num-cores 2 --send-external \
+# python crab.py -p pancakes_2018_data_UL_NANO.py -o /store/group/lpcbacon/pancakes/02/2018/UL/ -t pancakes-02 -i data_2018_UL.txt  --num-cores 2 --send-external \
 #     -s EventAwareLumiBased \
 #     -n 200000 --work-area crab_projects_data_2018 --max-memory 6000 \
 #     --json https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions18/13TeV/ReReco/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt --dryrun
@@ -29,7 +29,7 @@ python crab.py -p pancakes_2017_data_UL_NANO.py -o /store/group/lpcbacon/pancake
 #     --filein file:miniAOD.root --fileout file:nano_data_2016.root \
 #     --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring
 
-# python crab.py -p pancakes_2016_data_UL_NANO.py -o /store/group/lpcbacon/pancakes/02/2016/ -t pancakes-02 -i data_2016_UL.txt  --num-cores 2 --send-external \
+# python crab.py -p pancakes_2016_data_UL_NANO.py -o /store/group/lpcbacon/pancakes/02/2016/UL/ -t pancakes-02 -i data_2016_UL.txt  --num-cores 2 --send-external \
 #     -s EventAwareLumiBased \
 #     -n 200000 --work-area crab_projects_data_2016 --max-memory 6000 \
 #     --json https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_ReReco_07Aug2017_Collisions16_JSON.txt --dryrun
